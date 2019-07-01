74287b52896d78ee8205cf68db856cad5bebf9df
Rui Zhang <rui.zhang@vista.co>
2019-07-02 07:47:57 +1200

    add hook fragment up with navigation

--- app/src/main/java/co/ruizhang/weatherapp/views/CityListFragment.kt
+++ app/src/main/java/co/ruizhang/weatherapp/views/CityListFragment.kt
@@ -1,6 +1,13 @@
 package co.ruizhang.weatherapp.views
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import co.ruizhang.weatherapp.R
+import co.ruizhang.weatherapp.databinding.FragmentCityListBinding
 import co.ruizhang.weatherapp.viewmodels.CityListViewModel
 import co.ruizhang.weatherapp.viewmodels.ViewResultData
 import io.reactivex.disposables.CompositeDisposable
@@ -12,6 +19,12 @@ import timber.log.Timber
 class CityListFragment : Fragment() {
     private val cityListViewModel: CityListViewModel by viewModel()
     private var disposable: CompositeDisposable = CompositeDisposable()
+    private lateinit var binding: FragmentCityListBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentCityListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onStart() {
         super.onStart()
@@ -21,33 +34,50 @@ class CityListFragment : Fragment() {
                 onNext = { result ->
                     when (result) {
                         is ViewResultData.Loading -> {
+                            binding.emptyStateText.visibility = View.GONE
                             if (result.data.isNullOrEmpty()) {
                                 Timber.d("loading empty")
+                                binding.emptyStateProgress.visibility = View.VISIBLE
+                                binding.list.visibility = View.INVISIBLE
                             } else {
                                 Timber.d("loading with data")
+                                binding.emptyStateProgress.visibility = View.GONE
+                                binding.list.visibility = View.VISIBLE
                             }
                         }
                         is ViewResultData.Success -> {
+                            binding.emptyStateProgress.visibility = View.GONE
+
                             if (result.data.isNullOrEmpty()) {
                                 Timber.d("success empty")
+                                binding.emptyStateText.visibility = View.VISIBLE
+                                binding.list.visibility = View.INVISIBLE
                             } else {
                                 Timber.d("success with data")
+                                binding.emptyStateText.visibility = View.GONE
+                                binding.list.visibility = View.INVISIBLE
                             }
                         }
 
                         is ViewResultData.Error -> {
+                            binding.emptyStateProgress.visibility = View.GONE
+
                             if (result.data.isNullOrEmpty()) {
                                 Timber.d("Error empty")
+                                binding.emptyStateText.visibility = View.VISIBLE
+                                binding.list.visibility = View.INVISIBLE
                             } else {
-                                Timber.d("Error with data")
+                                Timber.d("Error with data ${result.throwable}")
+                                binding.emptyStateText.visibility = View.GONE
+                                binding.list.visibility = View.INVISIBLE
+                                Toast.makeText(context, R.string.city_error_text, Toast.LENGTH_LONG).show()
+
                             }
                         }
                     }
                 }
             )
-            .addTo(
-                disposable
-            )
+            .addTo(disposable)
     }
 
     override fun onStop() {

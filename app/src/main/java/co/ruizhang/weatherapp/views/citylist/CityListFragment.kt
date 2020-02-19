0cc831fc053d2e363bc25ebf30f036db7b9cd2db
Rui Zhang <rayruizhang1@gmail.com>
2020-02-19 22:03:24 +1300

    change searhc to use built-in activity to have better UX experience

--- app/src/main/java/co/ruizhang/weatherapp/views/citylist/CityListFragment.kt
+++ app/src/main/java/co/ruizhang/weatherapp/views/citylist/CityListFragment.kt
@@ -1,5 +1,8 @@
 package co.ruizhang.weatherapp.views.citylist
 
+import android.app.Activity.RESULT_CANCELED
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,23 +11,32 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import co.ruizhang.weatherapp.R
 import co.ruizhang.weatherapp.databinding.FragmentCityListBinding
 import co.ruizhang.weatherapp.viewmodels.CityListViewData
 import co.ruizhang.weatherapp.viewmodels.CityListViewModel
 import co.ruizhang.weatherapp.viewmodels.ViewResultData
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
+
 class CityListFragment : Fragment(), CityListClickListener {
     private val cityListViewModel: CityListViewModel by viewModel()
     private var disposable: CompositeDisposable = CompositeDisposable()
     private lateinit var binding: FragmentCityListBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = FragmentCityListBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -37,7 +49,7 @@ class CityListFragment : Fragment(), CityListClickListener {
         }
 
         binding.floatingActionButton.setOnClickListener {
-            findNavController().navigate(R.id.citySearchFragment)
+            openSearchActivity()
         }
         binding.recyclerView.adapter = CityListAdapter(this)
         binding.recyclerView.layoutManager = LinearLayoutManager(context)
@@ -79,12 +91,31 @@ class CityListFragment : Fragment(), CityListClickListener {
                             }
                             binding.emptyStateProgress.visibility = View.GONE
                             binding.swipeContainer.isRefreshing = false
-                            Toast.makeText(context, result.throwable.localizedMessage, Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                context,
+                                result.throwable.localizedMessage,
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
                     }
                 }
             )
             .addTo(disposable)
+    }
+
+    private fun openSearchActivity() {
+        // Set the fields to specify which types of place data to
+        // return after the user has made a selection.
+        // Set the fields to specify which types of place data to
+        // return after the user has made a selection.
+        val fields: List<Place.Field> =
+            listOf(Place.Field.ID, Place.Field.NAME)
+
+        // Start the autocomplete intent.
+        // Start the autocomplete intent.
+        val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, fields)
+            .build(context!!)
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE)
     }
 
     private fun showLoadedState(result: ViewResultData<List<CityListViewData>>) {
@@ -112,4 +143,25 @@ class CityListFragment : Fragment(), CityListClickListener {
     }
 
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            when (resultCode) {
+                RESULT_OK -> {
+                    val place = Autocomplete.getPlaceFromIntent(data!!)
+                    Timber.d("Place: ${place.name} , ${place.id}")
+                }
+                AutocompleteActivity.RESULT_ERROR -> { // TODO: Handle the error.
+                    val status: Status = Autocomplete.getStatusFromIntent(data!!)
+                    Timber.d(status.statusMessage)
+                }
+                RESULT_CANCELED -> { // The user canceled the operation.
+                    Timber.d("canceled")
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val AUTOCOMPLETE_REQUEST_CODE = 1
+    }
 }
